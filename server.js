1b226a2746e0e25f1a73d2dc968209915359af15
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-06-08 17:09:36 +0200

    Serwer usprawniony

--- server.js
+++ server.js
@@ -5,59 +5,47 @@ var socketio = require("socket.io");
 
 var server = http.createServer(function (req, res) {
 
-    var filesThatServerRequire = ['/libs/socket.io.js', '/libs/jquery.js', '/libs/three.js', '/libs/OrbitControls.js', '/js/Game.js', '/js/Net.js', '/js/UI.js', '/js/Main.js'];
+    var files = [
+        '/index.html',
+        '/css/style.css',
+        '/libs/socket.io.js',
+        '/libs/jquery.js',
+        '/libs/three.js',
+        '/libs/OrbitControls.js',
+        '/js/Game.js',
+        '/js/Net.js',
+        '/js/UI.js',
+        '/js/Main.js',
+    ];
 
     if (req.method == "GET") {
-
-
-        if (req.url === "/") {
+        if (files.includes(req.url)) {
+            getFile(req, res);
+        }
+        else {
             req.url = "/index.html"
-            getFile(req, res, "text/html")
-        }
-        else if (req.url == "/css/style.css") {
-            getFile(req, res, "text/css")
-        }
-        else if (req.url != "/") {
-            for (var i = 0; i < filesThatServerRequire.length; i++) {
-                if (req.url == filesThatServerRequire[i]) {
-                    getFile(req, res, filesThatServerRequire[i]);
-                }
-            }
+            getFile(req, res);
         }
     }
-
-    //AJAX somsiedzie
-    /*
-    if (req.method == "POST") {
-        var allData = "";
-        req.on("data", function (data) {
-            allData += data;
-        })
-        req.on("end", function (data) {
-            var finishObj = qs.parse(allData);
-        })
-    }
-    */
 })
 
-function getFile(req, res, type) {
+function getFile(req, res) {
     fs.readFile("static" + req.url, function (error, data) {
-        res.writeHead(200, { 'Content-Type': type });
+        res.writeHead(200);
         res.write(data);
         res.end();
     })
 }
 
 server.listen(3000, function () {
-    console.log("serwer startuje na porcie 3000")
+    console.log("PORT: 3000")
 });
-
 
 var io = socketio.listen(server)
 
 io.sockets.on("connection", function (client) {
 
-    console.log("klient się podłączył, ID: " + client.id)
+    console.log("CLIENT: " + client.id)
 
     client.emit("onconnect", {
         clientName: client.id
