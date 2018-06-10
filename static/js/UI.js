72d2bb09c8640e85eeef774619088d6153de6305
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-06-10 11:27:18 +0200

    Naprawa moich zabaw

--- static/js/UI.js
+++ static/js/UI.js
@@ -1,12 +1,15 @@
 function Ui() {
 
     var i = 0
-    var isTimerTickTock = false;
-    $("#timer")[0].innerHTML = "0 : 0 : 00";
+    var isTimerTickTock = false
+    $("#timer")[0].innerHTML = "00:00:00";
 
     $(document).keypress(function (e) {
         if (e.key = "Enter" && game.animation == false) {
 
+            if (isTimerTickTock == false) {
+                timer();
+            }
 
             var input_data = {
                 direction: 1,
@@ -33,4 +36,23 @@ function Ui() {
             i++;
         }
     });
+
+    function timer() {
+        var timeAtStartPoint = new Date().getTime()
+        isTimerTickTock = true;
+        setInterval(function () {
+            var timeInTimer = new Date().getTime();
+            var timeLeft = timeInTimer - timeAtStartPoint;
+            var secs = Math.floor((timeLeft % (60 * 1000)) / 1000)
+            var mins = Math.floor((timeLeft / (60 * 1000)))
+            var mili = Math.floor((timeLeft % 1000))
+            if (secs < 10) {
+                secs = "0" + secs;
+            }
+            if (mins < 10) {
+                mins = "0" + mins
+            }
+            $("#timer")[0].innerHTML = mins + ":" + secs + ":" + mili
+        }, 1)
+    }
 }
