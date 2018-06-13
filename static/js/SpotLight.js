465b9ee89beef55d152ff0e89a4bd137e65604ac
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-06-13 22:42:52 +0200

    Random move on Enter press

--- static/js/SpotLight.js
+++ static/js/SpotLight.js
@@ -1,5 +1,5 @@
 function SpotLight() {
-    var light = new THREE.SpotLight(0xffffff, 11, 1000, 2.5);
+    var light = new THREE.SpotLight(0xffffff, 9, 1000, 2.5);
     light.castShadow = true
     return light
 }
